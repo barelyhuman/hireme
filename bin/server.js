597adf8ac4ffda401912b7781a721cbb5a3c4fd4
Siddharth Gelera <sidgroks@gmail.com>
2020-04-03 01:03:59 +0530

    - exec the injectors - addition of comments

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,31 @@
 const { Cottage, Response } = require('cottage');
-const app = new Cottage();
+const bodyParser = require('koa-bodyparser');
 
 const PORT = process.env.PORT || 3000;
 
+// Custom Utils for Koa
+// if the require path didn't ring a bell
 const logger = require('../utils/logger');
 const jwtValidator = require('../utils/jwt-validator');
 const db = require('../utils/db-injector')();
 
+// If the names weren't obvious...
+// We importing injectors for routes
+const injectPrivateRoutes = require('../routes/private');
+const injectPublicRoutes = require('../routes/public');
+
+// Nope, not using the instance from Koa
+// Cottage on the other hand is for routing so I'm using it!
+// Deal with it!
+const app = new Cottage();
+
+// Inject minimal logger
 app.use(logger);
 
+// Parse requests
+app.use(bodyParser());
+
+// Inject db instance into the ctx
 app.use((ctx, next) => {
   ctx.db = db;
   next();
@@ -19,17 +36,18 @@ app.get('/ping', async (ctx) => {
   return new Response(200, `pong`);
 });
 
+injectPublicRoutes(app);
+
 /* 
   Routes that require the JWT to work 
   Can be added after the jwtValidator Insertion
 */
-app.use(jwtValidator());
-app.get('/pong', async (ctx) => {
-  return new Response(200, `pong`);
-});
 
+app.use(jwtValidator());
+
+injectPrivateRoutes(app);
+
+// Run the engines!
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
-
-module.exports = app;
